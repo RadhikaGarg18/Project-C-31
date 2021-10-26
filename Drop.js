bb278392208db8376757e7c329b5c662f7cfd284
RadhikaGarg18 <72866344+RadhikaGarg18@users.noreply.github.com>
2021-10-26 16:51:44 +0530

    Add files via upload

--- Drop.js
+++ Drop.js
@@ -5,22 +5,23 @@ class Drop{
             restitution: 0.3,
             density: 0.04
         }
+            
+            this.body=Bodies.circle(this.x, this.y, this.r, options);
             this.x = x;
             this.y = y;
             this.r = r;
-            this.body=Bodies.circle(this.x, this.y, this.r, options)
             World.add(world, this.body);
     }
     showDrop(){
         fill("blue");
         ellipseMode(CENTER);
-        ellipse( this.body.position.x,this.body.position.y,this.r,this.r);
+        ellipse( this.body.position.x,this.body.position.y,this.r);
                 
        
     }
     updateY(){
     if(this.body.position.y > height){
-        Matter.Body.setPosition(this.body,{x: random(0,500),y:random(0,0)})
+        Matter.body.setPosition(this.body,{x: random(0,500),y:random(0,0)})
     }
 }
 }
